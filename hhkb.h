723091235ae6d190f23de604925162289f06077f
Douglas Stockwell <doug@11011.net>
2014-11-15 12:20:56 +1100

    Move hhkb logic to a separate file

---
+++ hhkb.h
@@ -0,0 +1,67 @@
+// See: https://github.com/tmk/tmk_keyboard/blob/master/keyboard/hhkb/doc/HHKB.txt
+
+class hhkb {
+public:
+  hhkb() 
+    : m_matrix(0)
+    , m_prevMatrix(0)
+  {
+  }
+
+  uint64_t matrix() const { return m_matrix; }
+  uint64_t prevMatrix() const { return m_prevMatrix; }
+  bool scan() {
+    m_prevMatrix = m_matrix;
+    m_matrix = 0;
+    for (uint8_t row = 0; row < 8; row++) {
+      for (uint8_t col = 0; col < 8; col++) {
+        select(row, col);
+        delayMicroseconds(15);
+        enable();
+        delayMicroseconds(5);
+  
+        if (read()) {
+          m_matrix |= UINT64_C(1) << (row * 8 + col);
+        }
+  
+        delayMicroseconds(5);
+        disable();
+        delayMicroseconds(75);
+      }
+    }
+    return m_matrix != m_prevMatrix;
+  }
+
+  void init() {
+    pinMode(0, INPUT_PULLUP);
+    for (uint8_t i = 1; i < 9; i++) {
+      pinMode(i, OUTPUT);
+    }
+  }
+
+private:
+  uint64_t m_matrix;
+  uint64_t m_prevMatrix;
+
+  void select(int row, int col) {
+    digitalWriteFast(2, row & 1 ? HIGH : LOW);
+    digitalWriteFast(3, row & 2 ? HIGH : LOW);
+    digitalWriteFast(4, row & 4 ? HIGH : LOW);
+  
+    digitalWriteFast(5, col & 1 ? HIGH : LOW);
+    digitalWriteFast(6, col & 2 ? HIGH : LOW);
+    digitalWriteFast(7, col & 4 ? HIGH : LOW);
+  }
+  
+  void enable() {
+    digitalWriteFast(8, LOW);
+  }
+  
+  void disable() {
+    digitalWriteFast(8, HIGH);
+  }
+  
+  bool read() {
+    return digitalReadFast(0) == LOW;
+  }
+};
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "WProgram.h"
 #include "usb_dev.h"
+#include "hhkb.h"
 
 uint8_t keys[KEYBOARD_SIZE] = {0};
 void send();
@@ -11,32 +12,6 @@ void blink() {
   delay(16);
 }
 
-// See: https://github.com/tmk/tmk_keyboard/blob/master/keyboard/hhkb/doc/HHKB.txt
-void select(int row, int col) {
-  digitalWriteFast(2, row & 1 ? HIGH : LOW);
-  digitalWriteFast(3, row & 2 ? HIGH : LOW);
-  digitalWriteFast(4, row & 4 ? HIGH : LOW);
-
-  digitalWriteFast(5, col & 1 ? HIGH : LOW);
-  digitalWriteFast(6, col & 2 ? HIGH : LOW);
-  digitalWriteFast(7, col & 4 ? HIGH : LOW);
-}
-
-void enable() {
-  digitalWriteFast(8, LOW);
-}
-
-void disable() {
-  digitalWriteFast(8, HIGH);
-}
-
-bool read() {
-  return digitalReadFast(0) == LOW;
-}
-
-uint8_t matrix_prev[8] = {0};
-uint8_t matrix[8] = {0};
-
 uint8_t keymap[8][8] = {
  {  39,  28,  34,  30,  12,  37,  35,  14},  
  {  40,  41,  29,  16,  15,  17,  33,  13},  
@@ -47,6 +22,18 @@ uint8_t keymap[8][8] = {
  {  46,  47,  26,  27,   0,  23,   0,   0},  
  {   0,   0,   0,   0,   0,   0,   0,   0},  
 };
+
+bool getBit(uint64_t vector, uint8_t offset) {
+  return (vector & (UINT64_C(1) << offset)) > 0;
+}
+
+void putBit(uint64_t& vector, uint8_t offset, bool value) {
+  if (value) {
+    vector |= 1 << offset;
+  } else {
+    vector &= ~(1 << offset);
+  }
+}
 
 bool getBit(uint8_t buffer[], uint8_t offset) {
   uint8_t row = offset / 8;
@@ -68,59 +55,26 @@ void reboot() {
   __asm__ volatile("bkpt");
 }
 
-void scan() {
-  for (uint8_t row = 0; row < 8; row++) {
-    matrix_prev[row] = matrix[row];
-    for (uint8_t col = 0; col < 8; col++) {
-      select(row, col);
-      delayMicroseconds(15);
-      enable();
-      delayMicroseconds(5);
-
-      if (read()) {
-        matrix[row] |= 1 << col;
-      } else {
-        matrix[row] &= ~(1 << col);
-      }
-
-      delayMicroseconds(5);
-      disable();
-      delayMicroseconds(75);
-    }
-  }
-  bool changed = false;
-  for (uint8_t row = 0; row < 8; row++) {
-    if (matrix[row] != matrix_prev[row]) {
-      changed = true;
-      break;
-    }
-  }
-  uint64_t thing = 0;
-  if (changed) {
-    for (uint8_t row = 0; row < 8; row++) {
-      for (uint8_t col = 0; col < 8; col++) {
-        putBit(keys, keymap[row][col], getBit(matrix, row * 8 + col));
-      }
-    }
-    if (getBit(matrix, 1) && getBit(matrix, 4) && getBit(matrix, 5)) {
-      delay(500);
-      reboot();
-    }
-    send();
-  }
-}
-
 extern "C" int main(void)
 {
-  pinMode(0, INPUT_PULLUP);
-  for (uint8_t i = 1; i < 9; i++) {
-  pinMode(i, OUTPUT);
-  }
   pinMode(13, OUTPUT);
 
-  blink();
+  hhkb keyboard;
+  keyboard.init();
+
   while (1) {
-    scan();
+    if (keyboard.scan()) {
+      uint64_t matrix = keyboard.matrix();
+      for (uint8_t row = 0; row < 8; row++) {
+        for (uint8_t col = 0; col < 8; col++) {
+          putBit(keys, keymap[row][col], getBit(matrix, row * 8 + col));
+        }
+      }
+      if (getBit(matrix, 1) && getBit(matrix, 4) && getBit(matrix, 5)) {
+        reboot();
+      }
+      send();
+    }
   }
 }
 
