199a5c6e475fadca89b0c4ab64bbf9248a671460
Douglas Stockwell <doug@11011.net>
2014-11-16 11:31:28 +1100

    Make the mapping readable

--- app/index.js
+++ app/index.js
@@ -22,16 +22,45 @@ var keyMessage = new Uint8Array(64);
 keyMessage[0] = 2;
 var keys = keyMessage.subarray(1, 17);
 
-var keymap = [
-  [39, 28, 34, 30, 12, 37, 35, 14],
-  [40, 41, 29, 16, 15, 17, 33, 13],
-  [42, 43, 36, 31, 18, 19, 25,  0],
-  [38, 49, 51,  4,  1,  2,  3, 52],
-  [44, 45, 32, 20, 22, 21, 24,  0],
-  [58, 61, 50, 48,  0,  5,  6,  7],
-  [46, 47, 26, 27, 59, 23, 62,  0],
-  [53, 54, 56, 55, 60, 64, 63,  0],
+var hhkbKeys = [
+  '2', 'q', 'w', 's', 'a', 'z', 'x', 'c',
+  '3', '4', 'r', 'e', 'd', 'f', 'v', 'b',
+  '5', '6', 'y', 't', 'g', 'h', 'n', null,
+  '1', 'Esc', 'Tab', 'LControl', 'LShift', 'LAlt', 'LMeta', 'Space',
+  '7', '8', 'u', 'i', 'k', 'j', 'm', null,
+  '\\', '`', 'Backspace', 'Return', 'Fn', 'RShift', 'RAlt', 'RMeta',
+  '9', '0', 'o', 'p', ';', 'l', ',', null,
+  '-', '+', ']', '[', "'", '/', '.', null,
 ];
+
+var hhkbIndexes = {};
+hhkbKeys.forEach(function(k, i) {
+  if (k != null) hhkbIndexes[k] = i;
+});
+
+var nkroKeys = [
+  'LControl', 'LShift', 'LAlt', 'LMeta', 'RControl', 'RShift', 'RAlt', 'RMeta',
+  null, null, null, null,'a', 'b', 'c', 'd',
+  'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+  'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+  'u', 'v', 'w', 'x', 'y', 'z', '1', '2',
+  '3', '4', '5', '6', '7', '8', '9', '0',
+  'Return', 'Esc', 'Backspace', 'Tab', 'Space', '-', '+', '[',
+  ']', null, '\\', ';', "'", '`', ',',
+  '.', '/', 'CapsLock', 'F1', 'F2', 'F3', 'F4', 'F5',
+  'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'PrintScreen',
+  'ScrollLock', 'Pause', 'Insert', 'Home', 'PageUp', 'Delete', 'End', 'PageDown',
+  'RightArrow', 'LeftArrow', 'DownArrow', 'UpArrow',
+];
+
+var nkroIndexes = {};
+nkroKeys.forEach(function(k, i) {
+  if (k != null) nkroIndexes[k] = i;
+});
+
+var keymap = hhkbKeys.map(function(k) {
+  return nkroIndexes[k]|0;
+});
 
 function getBit(vector, offset) {
   var byte = offset / 8|0;
@@ -51,20 +80,20 @@ function setBit(vector, offset, value) {
 
 function onreport(report, data) {
   var matrix = new Uint8Array(data.slice(0, 8));
-  var s = '';
-  for (var i = 0; i < 8; i++) {
-    var bits = matrix[i].toString(2);
-    s += ('00000000'.substring(0, 8 - bits.length) + bits).split('').reverse().join('');
-  }
-  for (var row = 0; row < 8; row++) {
-    for (var col = 0; col < 8; col++) {
-      setBit(keys, keymap[row][col], getBit(matrix, row * 8 + col));
+  var pressedKeys = [];
+  for (var i = 0; i < 64; i++) {
+    var pressed = getBit(matrix, i);
+    setBit(keys, keymap[i], pressed);
+    if (pressed) {
+      pressedKeys.push(hhkbKeys[i]);
     }
   }
+  setTimeout(function() {
   chrome.hid.send(connectionId, 0, keyMessage.buffer, function() {});
+}, 20);
 
   var time = new Uint32Array(data.slice(8, 12));
-  s += ' ' + (time[0] / 1000);
+  var s = (time[0] / 1000) + ' ' + pressedKeys.join('+');
 
   log.unshift(s);
   while (log.length > 20) log.pop();
